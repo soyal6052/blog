1918d2e2ab9b3ad210f3c9ff334d2d1d07b48b5d
soyal6052 <soyalahimad1996@gmail.com>
2020-04-04 17:31:16 +0530

    added user api

--- src/api-v1.ts
+++ src/api-v1.ts
@@ -7,7 +7,9 @@ app.get('/test', async (req, res) => {
     
     res.json({hello : "hi"});
 })
-
+app.get('/user',async (req,res) => {
+res.json({name :"soyal"});
+} )
 export {
     app as apiV1
 }
