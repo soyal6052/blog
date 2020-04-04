5499e609ec854871b75667774ede9bbe0546d199
sonusharma07 <meetsonusharma07@gmail.com>
2020-04-04 16:25:13 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-//configuration
+import * as express from 'express';
+
+import { apiV1 } from './api-v1';
+const app = express();
+
+const PORT = Number(process.env.PORT) || 8080;
+app.use('/api/v1', apiV1);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
